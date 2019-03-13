3585ddadd5a29959672684a32fbea4e6ec45df15
Veli Bacık <velibacik60@gmail.com>
2019-03-14 01:06:42 +0300

    design fix 1

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:manhang/model/BaseEnum.dart';
+import 'package:manhang/model/Question.dart';
+import '../model/BaseEnum.dart';
 
 class GamePage extends StatefulWidget {
   GamePage({
@@ -11,13 +14,18 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  final _tfController = new TextEditingController();
+  final _textEC = new TextEditingController();
+  final String word = "";
+
   String questionWord = "";
-  final String word = "test";
+  String questionDescription = "";
+
   int wrongAnswer = 1;
+  final int totalRights = 5;
+
+  bool _isValid = true;
+
   List<String> _listWord = new List<String>();
-  List<String> _errorList = ["1", "2", "3", "4", "5"];
-  HumanModel model = new HumanModel();
 
   pigLatin(String words) => words.replaceAllMapped(
       new RegExp(r'([A-Z])', caseSensitive: false), (Match m) => "_ ");
@@ -25,32 +33,44 @@ class _GamePageState extends State<GamePage> {
   @override
   void initState() {
     super.initState();
+    final int _randomNumber = new Random().nextInt(LANGUAGE.values.length - 1);
+    Question _question = new Question();
+    final LANGUAGE randomLanguage = LANGUAGE.values[_randomNumber];
     setState(() {
-      questionWord = pigLatin(word);
+      questionWord = pigLatin(randomLanguage.toString().split(".").last);
+
+      questionDescription = _question.languageWords[randomLanguage];
       _listWord = questionWord.trim().split(" ");
     });
   }
 
   void onPressOfButton() {
-    int searchIndex = word.indexOf(_tfController.text);
+    setState(() {
+      _isValid = _textEC.text.isEmpty ? true : false;
+    });
+    if (!_isValid) {
+      return;
+    }
+
+    int searchIndex = word.indexOf(_textEC.text);
 
     if (searchIndex >= 0) {
       //end game.
       if (!questionWord.contains("_")) {
         endGame(true);
       } else {
-        if (questionWord.contains(_tfController.text)) {
+        if (questionWord.contains(_textEC.text)) {
           return;
         } else {
           convertList();
         }
       }
     } else {
-      _tfController.text = "";
+      _textEC.text = "";
       setState(() {
         wrongAnswer++;
       });
-      if (wrongAnswer == _errorList.length) {
+      if (wrongAnswer == totalRights) {
         endGame(false);
       }
       return;
@@ -71,8 +91,8 @@ class _GamePageState extends State<GamePage> {
 
   void convertList() {
     for (var i = 0; i < _listWord.length; i++) {
-      if (word[i] == _tfController.text) {
-        _listWord[i] = _tfController.text;
+      if (word[i] == _textEC.text) {
+        _listWord[i] = _textEC.text;
       }
     }
     var _newList = _listWord.map((data) => "$data ");
@@ -81,7 +101,7 @@ class _GamePageState extends State<GamePage> {
     setState(() {
       questionWord = _questionWord;
     });
-    _tfController.text = "";
+    _textEC.text = "";
     if (!questionWord.contains("_")) {
       endGame(true);
     }
@@ -90,50 +110,68 @@ class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.red,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Color.fromRGBO(255, 46, 99, 1),
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('assets/images/$wrongAnswer.png'),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
                 alignment: Alignment.bottomCenter,
-                child: Image.asset('assets/images/$wrongAnswer.png'),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                  alignment: Alignment.bottomCenter,
-                  color: Colors.black,
-                  child: SafeArea(
-                      child: Column(
-                    children: <Widget>[
-                      Text(
-                        questionWord,
-                        style: Theme.of(context).textTheme.title,
-                        softWrap: false,
-                      ),
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                        alignment: Alignment.center,
-                        child: TextField(
-                          controller: _tfController,
-                          textAlign: TextAlign.center,
-                          maxLength: 1,
+                color: Color.fromRGBO(34, 40, 49, 1),
+                child: SafeArea(
+                    child: Column(
+                  children: <Widget>[
+                    Wrap(
+                      alignment: WrapAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.help,
+                          color: Color.fromRGBO(234, 234, 234, 1),
+                          size: 45,
                         ),
+                        Text(
+                          questionDescription.toUpperCase(),
+                          softWrap: true,
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(255, 46, 99, 1)),
+                        )
+                      ],
+                    ),
+                    Text(
+                      questionWord,
+                      style: Theme.of(context).textTheme.title,
+                      softWrap: false,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: TextField(
+                        controller: _textEC,
+                        textAlign: TextAlign.center,
+                        maxLength: 1,
+                        
+                        decoration: InputDecoration(
+                            icon: Icon(Icons.keyboard),
+                            hintText: "Try your idea"),
+                            
                       ),
-                      RaisedButton(
-                        child: new Text("Update"),
-                        onPressed: onPressOfButton,
-                      ),
-                    ],
-                  ))),
-            ),
-          ],
-        ),
+                    ),
+                    RaisedButton(
+                      child: Text("TRY"),
+                      onPressed: onPressOfButton,
+                    ),
+                  ],
+                ))),
+          ),
+        ],
       ),
     );
   }
