e4249bb350637f4ba930b2b77e5b8f5bebbb7f1f
Veli Bacık <velibacik60@gmail.com>
2019-03-15 01:57:38 +0300

    pubspec fix and unit test start

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,25 +1,19 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:manhang/main.dart';
+import 'package:manhang/screens/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MainPage());
-
-  
-    // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    await tester.pumpWidget(LoginPage());
+    // Tap the '+' icon and trigger a frame.
+    expect(find.byIcon(Icons.inbox), findsOneWidget);
+  });
+  test('AssetImage from package', () {
+    const AssetImage image = AssetImage(
+      'assets/gif/stck.gif',
+    );
+    expect(image.keyName, 'assets/gif/stck.gif');
   });
 }
