5b42b53586367995bd8c9e3d5525d6466a9c4137
Veli Bacık <velibacik60@gmail.com>
2019-03-16 00:07:28 +0300

    login test

--- test/login_test.dart
+++ test/login_test.dart
@@ -4,12 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:manhang/screens/login.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(LoginPage());
-    // Tap the '+' icon and trigger a frame.
-    expect(find.byIcon(Icons.inbox), findsOneWidget);
-  });
   test('AssetImage from package', () {
     const AssetImage image = AssetImage(
       'assets/gif/stck.gif',
