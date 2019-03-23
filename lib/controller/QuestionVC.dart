f515ae1f91e72466ceebf81aa74793dd9dc397fd
Veli Bacık <velibacik60@gmail.com>
2019-03-24 00:59:17 +0300

    fix 5.172 kb

--- lib/controller/QuestionVC.dart
+++ lib/controller/QuestionVC.dart
@@ -1,22 +1,24 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:manhang/main.dart';
 import 'package:manhang/model/BaseEnum.dart';
 import 'package:manhang/model/Question.dart';
 
 class QuestionVC {
   Question questionModel = new Question();
-  BuildContext context;
+  static final QuestionVC _questionVC = new QuestionVC();
+  static QuestionVC get instance => _questionVC;
 
-  QuestionVC(this.context) {
-    setLanguageWord();
+  QuestionVC() {
+    questionModel.languageWords[LANGUAGE.dart] =
+        "helps you craft beautiful, high-quality experiences across all screens";
     questionModel.language = getRandomLanguage();
     questionModel.questionWord = pigUnderline(questionModel.language);
     questionModel.listWord = questionModel.questionWord.trim().split(" ");
   }
-
   String getRandomLanguage() {
-    final int _randomNumber = new Random().nextInt(LANGUAGE.values.length - 1);
-    final LANGUAGE randomLanguage = LANGUAGE.values[_randomNumber];
+    final LANGUAGE randomLanguage =
+        LANGUAGE.values[new Random().nextInt(LANGUAGE.values.length - 1)];
     questionModel.questionDescription =
         questionModel.languageWords[randomLanguage];
     return randomLanguage.toString().split(".").last;
@@ -28,11 +30,11 @@ class QuestionVC {
         questionModel.listWord[i] = inputValue;
       }
     }
-    var _newList = questionModel.listWord.map((data) => "$data ");
-    return _newList.join("");
+    return questionModel.listWord.map((data) => "$data ").join("");
   }
 
-  void endGame(bool type) {
+  void endGame(bool type, BuildContext context) {
+    Route route = MaterialPageRoute(builder: (context) => GamePage());
     showDialog(
         context: context,
         builder: (BuildContext context) => new AlertDialog(
@@ -40,38 +42,22 @@ class QuestionVC {
                 "assets/gif/${type ? "tenor" : "fail"}.gif",
               ),
               backgroundColor: Colors.transparent,
-            )).whenComplete(() => {Navigator.pop(context, true)});
+            )).whenComplete(() => {Navigator.pushReplacement(context, route)});
     return;
   }
 
   pigUnderline(String words) => words.replaceAllMapped(
       new RegExp(r'([A-Z])', caseSensitive: false), (Match m) => "_ ");
 
-  setLanguageWord() {
-    questionModel.languageWords[LANGUAGE.dart] =
-        "helps you craft beautiful, high-quality experiences across all screens";
-    questionModel.languageWords[LANGUAGE.go] =
-        "that makes it easy to build simple, reliable, and efficient software.";
-    questionModel.languageWords[LANGUAGE.java] =
-        "is at the heart of our digital lifestyle.";
-    questionModel.languageWords[LANGUAGE.swift] =
-        "Our goals for Swift are ambitious: we want to make programming simple things easy, and difficult things possible";
-    questionModel.languageWords[LANGUAGE.python] =
-        " is a programming language that lets you work quickly and integrate systems more effectively";
-  }
-
-  String userOnPress(String input) {
-    int searchIndex = questionModel.language.indexOf(input);
-    if (searchIndex >= 0) {
-      if (!questionModel.questionWord.contains("_")) {
-        endGame(true);
-      } else {
+  String userOnPress(String input, BuildContext context) {
+    if (questionModel.language.indexOf(input) >= 0) {
+      if (!questionModel.questionWord.contains("_"))
+        endGame(true, context);
+      else {
         if (!questionModel.questionWord.contains(input)) {
           return convertList(input);
         }
       }
-    } else {
-      return null;
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:manhang/controller/QuestionVC.dart';
 import 'package:manhang/model/BaseEnum.dart';
-import 'package:manhang/screens/game.dart';
 
 void main() => runApp(MainPage());
 
@@ -9,49 +9,84 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // Define the default Brightness and Colors
-        brightness: Brightness.dark,
-        fontFamily: 'Montserrat',
-        textTheme: TextTheme(
-          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-        ),
-      ),
-      home: Scaffold(body: LoginPage()),
+      theme: ThemeData.dark(),
+      home: GamePage(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  //Fab Button press and route hero animaton game page.
-  onPress(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (_) {
-      return GamePage();
-    }));
+class GamePage extends StatefulWidget {
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  final _textEC = new TextEditingController();
+  String inputText = "";
+  int wrongAnswer = 1;
+  QuestionVC questionVC = QuestionVC.instance;
+  String questionWord = QuestionVC.instance.questionModel.questionWord;
+
+  tryOnPress() {
+    if (inputText.isEmpty) return;
+    var isOkey =
+        questionVC.userOnPress(inputText, this.context) == null ? true : false;
+    setState(() {
+      wrongAnswer += isOkey ? 1 : 0;
+      questionWord = !isOkey ? questionVC.convertList(inputText) : questionWord;
+    });
+    if (wrongAnswer == questionVC.questionModel.totalRights) {
+      questionVC.endGame(false, this.context);
+      return;
+    }
+    if (!questionWord.contains("_")) {
+      questionVC.endGame(true, this.context);
+      return;
+    }
+    _textEC.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Hero(
             tag: TAG.game.toString(),
             child: Image.asset(
-              "assets/gif/stck.gif",
+              'assets/images/$wrongAnswer.png',
+              height: MediaQuery.of(context).size.height * 0.5,
             ),
           ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: FloatingActionButton(
-              onPressed: () => this.onPress(context),
-              child: Icon(Icons.arrow_forward_ios),
-            ),
-          )
+          Icon(
+            Icons.help,
+          ),
+          Text(
+            questionVC.questionModel.questionDescription,
+          ),
+          Text(
+            questionWord,
+            style: TextStyle(fontSize: 40),
+          ),
+          TextField(
+            controller: _textEC,
+            maxLength: 1,
+            onChanged: (val) => setState(() {
+                  inputText = val;
+                }),
+            onEditingComplete: tryOnPress,
+            decoration: InputDecoration(
+                icon: Icon(Icons.keyboard), hintText: "Try word your thing"),
+          ),
+          RaisedButton(
+            child: Text("TRY"),
+            onPressed: tryOnPress,
+          ),
         ],
-      ),
+      )),
     );
   }
 }
